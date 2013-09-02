69b5eec9f4f41b026447d5c8570ce8b4c0609c8c
Jonathan Jesneck <jonathan.jesneck@gmail.com>
2013-09-01 22:37:38 -0400

    starting to make the plots

--- analyze_data.py
+++ analyze_data.py
@@ -15,7 +15,7 @@ from database import Database
 def main():
     config = parse_config_file()
     print config
-    # Extract the database connection info from the configuation info.
+    # Extract the database connection info from the configuration info.
     db_info = config['mysql']
     host = db_info['host']
     user = db_info['user']
@@ -32,21 +32,66 @@ def calculate_basic_stats(db):
     cases = db.get_cases()
     print 'found', len(cases), 'cases'
     print cases[0]
-    counts = count_resident_cases(cases)
+    # counts = count_resident_cases(cases)
+    count_resident_cases(db)
 
 
+def count_resident_cases(db):
+    """
+    Count the resident cases.
 
-def count_resident_cases(cases):
-    res_cols = [1, 2]
-    res_names = [make_resident_name(case[2], case[1]) for case in cases]
-    c = Counter(res_names)
-    print 'Resident\tCase count'
-    for res, count in c.most_common():
-        print res + '\t' + str(count)
-    return c
+    :param db: database object
+    :type db:  Database class
+    :return:
+    :rtype:
+    """
+    # res_cols = [1, 2]
+    # res_names = [make_resident_name(case[2], case[1]) for case in cases]
+    # c = Counter(res_names)
+    # print 'Resident\tCase count'
+    # for res, count in c.most_common():
+    #     print res + '\t' + str(count)
+
+    # Define the CPT code groups by operation type.
+    cpt_groups = {
+        'appy': [44970, 44950, 44960],
+        'open_colectomy': [44140, 44141, 44143, 44144, 44145, 44146, 44150, 44151, 44155, 44156, 44160],
+        'lap_colectomy': [44204, 44206, 44207, 44208, 44211, 44212],
+        'lap_choley': [47562, 47563, 47600, 47605],
+        'open_inguinal_hernia': [49505, 49507, 49520, 49521, 49525],
+        'lap_inguinal_hernia': [49650, 49651]
+    }
+    # for group_name, cpt_codes in cpt_groups.iteritems():
+    #     # Query for the number of cases in the current CPT group.
+    #     results = db.count_cpt_group_by_resident(cpt_codes)
+    #     for result in results:
+    #         res_first_name, res_last_name, num_cases = result
+    #         print group_name, res_first_name, res_last_name, num_cases
+    # Do the same query, but by pgy this time.
+    print '\t'.join(['CPT group', 'First name', 'Last name', 'PGY', 'Num cases'])
+    for group_name, cpt_codes in cpt_groups.iteritems():
+        # Query for the number of cases in the current CPT group.
+        results = db.count_cpt_group_by_resident(cpt_codes, by_pgy=True)
+        for result in results:
+            res_first_name, res_last_name, pgy, num_cases = result
+            print '\t'.join([group_name, res_first_name, res_last_name, str(pgy), str(num_cases)])
+
+    # return c
+
 
 def make_resident_name(first, last):
+    """
+    Combine the resident name parts to make the full name.
+
+    :param first: resident's first name
+    :type first: str
+    :param last: resident's last name
+    :type last: str
+    :return: resident's full name
+    :rtype: str
+    """
     return first.strip() + ' ' + last.strip()
+
 
 def count_by_resident(cases, col_nums, res_name_cols=[2, 1]):
     res_names = [make_resident_name(case[res_name_cols[0]], case[res_name_cols[1]])
--- database.py
+++ database.py
@@ -4,11 +4,22 @@ import MySQLdb as mdb
 class Database(object):
 
     def __init__(self, host, user, passwd, db_name):
+        """
+        :param host: mysql server hostname
+        :type host: str
+        :param user: mysql user name
+        :type user: str
+        :param passwd: password
+        :type passwd: str
+        :param db_name: database name
+        :type db_name: str
+        """
         self.host = host
         self.user = user
         self.db_name = db_name
         self.con = None
         self.db = None
+        # Connect to the mysql database.
         self.connect_to_mysql(self.host, self.user, passwd, self.db_name)
 
     def __del__(self):
@@ -22,7 +33,7 @@ class Database(object):
 
     def get_num_cases(self, complete=True):
         if complete:
-            q = 'select count(*) from cases, where overall_value > 0'
+            q = 'select count(*) from cases where overall_val > 0'
         else:
             q = 'select count(*) from cases'
         self.cur.execute(q)
@@ -31,7 +42,7 @@ class Database(object):
 
     def get_cases(self, complete=True):
         if complete:
-            q = 'select * from cases where overall_value > 0'
+            q = 'select * from cases where overall_val > 0'
         else:
             q = 'select * from cases'
         self.cur.execute(q)
@@ -46,6 +57,28 @@ class Database(object):
 
     def count_cpt_by_resident(self):
         q = 'select resident_first_name, resident_last_name, cpt_code, count(cpt_code) as num_cases from cases group by resident_first_name, resident_last_name, cpt_code order by num_cases desc'
+        self.cur.execute(q)
+        result = self.cur.fetchall()
+        return result
+
+    def count_cpt_group_by_resident(self, cpt_group, by_pgy=False):
+        """
+        :param cpt_group: the cpt codes in the group
+        :type cpt_group: list of strings
+        :return: database cursor of the query results
+        :rtype: mysql database cursor
+        """
+        # Make sure the collection is a tuple, so that the string version is flanked by parentheses for mysql.
+        cpt_group_str = str(tuple(cpt_group))
+        # Make the query string.
+        if by_pgy:
+            # Group by the resident and by PGY.
+            q = 'select resident_first_name, resident_last_name, pgy, count(*) as num_cases from cases where cpt_code in ' \
+                + cpt_group_str + ' group by resident_first_name, resident_last_name, pgy order by num_cases desc'
+        else:
+            # Group by the resident.
+            q = 'select resident_first_name, resident_last_name, count(*) as num_cases from cases where cpt_code in ' \
+                + cpt_group_str + ' group by resident_first_name, resident_last_name order by num_cases desc'
         self.cur.execute(q)
         result = self.cur.fetchall()
         return result
